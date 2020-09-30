74e15df19c42b67a79beb14aab64fff955f3558a
beejgill <71659662+beejgill@users.noreply.github.com>
2020-09-30 16:13:45 -0600

    Update finalmaster.py

--- finalmaster.py
+++ finalmaster.py
@@ -3,8 +3,7 @@
 class customer():
     company_name = 'Critter Watch'
 
-    def __init__ (self,first_name, last_name, address1, address2, city, state, sZip):
-        self.cust_id = self.gen_id()
+    def __init__ (self, first_name, last_name, address1, address2, city, state, sZip):
         self.first_name = first_name
         self.last_name = last_name
         self.address1 = address1
@@ -13,18 +12,19 @@ class customer():
         self.state = state
         self.sZip = sZip
         self.balance = 0.0
+        self.cust_id = self.gen_id(first_name, last_name, address1)
         self.cust_pet = None
 
     def gen_id(self,first_name,last_name,address1):
         cust_id = (self.first_name[0 : 3] + self.last_name[0 : 3] + self.address1[0 : 5])
         return(cust_id)
 
-    def return_bill(self, cust_id, first_name, last_name, balance, cust_pet, Appointment.begin_date, Appointment.end_date):
-        return('Customer ' + cust_id + 'with name ' + first_name + ' ' + last_name + ' owes $' + balance + ' for ' + cust_pet.pet_name + "'s stay from " + Appointment.begin_date + " to " + Appointment.end_date )
+    def return_bill(self):
+        return('Customer ' + self.cust_id + ' with name ' + self.first_name + ' ' + self.last_name + ' owes $' + str(self.balance) + ' for ' + self.cust_pet.pet_name + "'s stay from " + str(self.cust_pet.appointment.dBegin_date) + " to " + str(self.cust_pet.appointment.dEnd_date))
 
     def make_payment(self, fPayment):
-        balance = balance - fPayment
-        return(balance)
+        self.balance = self.balance - fPayment
+        return(self.balance)
         
         #create Class o Instance Variables:  pet_name is of type string,  breed is of type string,  age is of type int,  appointment of type Appointment
 class pet():
@@ -33,16 +33,8 @@ class pet():
         self.pet_name = pet_name
         self.breed = breed
         self.age = iAge
-        #self.appointment = appointment(owner) #When calling the Appointment constructor you will want to pass the owner
+        self.appointment = Appointment(owner) #When calling the Appointment constructor you will want to pass the owner
 
-   #  pet constructor  
-    def pet (self, pet_name, breed, age, owner):
-        self.appointment = appointment(owner) #When calling the Appointment constructor you will want to pass the owner
-
-    #pet method    
-        oCustomer.cust_pet = Pet(self.pet_name, self.breed, self.iAge, self.oCustomer)
-        return(oCustomer.cust_pet)
-        from datetime import datetime
 class Appointment():
 
     def __init__(self, owner):
@@ -54,17 +46,17 @@ class Appointment():
         self.iTotal_cost = 0.0
         self.oOwner = owner
 
-    def set_appointment(begin_date, end_date, day_rate):
+    def set_appointment(self, begin_date, end_date, day_rate):
         self.dBegin_date = begin_date
         self.dEnd_date = end_date
         self.fDay_rate = day_rate
 
-        calc_days()                                                 # calls calc_days() function
+        self.calc_days()                                                 # calls calc_days() function
 
         #why are we assigning values to iTotal_cost here AND down in calc_days()?
-        self.iTotal_cost = self.owner.balance                       # updates the balance
+        self.oOwner.balance = self.iTotal_cost + self.oOwner.balance                       # updates the balance
 
-    def calc_days():
+    def calc_days(self):
         self.iTotal_days = (self.dEnd_date - self.dBegin_date).days # calculates total_days (subtracting end_date from begin_date)
            
         if self.iTotal_days <= 0 :                                  # checks if total_days is less than or equal to 0
@@ -73,10 +65,6 @@ class Appointment():
         self.iTotal_cost = self.iTotal_days * self.fDay_rate        # calculates the total_cost (multiplying total_days by day_rate)
 
         return self.iTotal_days                                     # returns the days value
-        from datetime import datetime
-
-#  class variable within customer class
-company_name = "Critter Watch"
 
 first_name = input("What is your first name? ")
 last_name = input("What is your last name? ")
@@ -86,27 +74,24 @@ city = input("What city do you live in? ")
 state = input("What state do you live in? ")
 zip_code = input("What is your zip code? ")
 
-customer = Customer(first_name, last_name, address1, address2, city, state, zip_code)
+oCustomer = customer(first_name, last_name, address1, address2, city, state, zip_code)
 
 pet_name = input("What is your pet's name? ")
 pet_breed = input("What is your pet's breed? ")
 pet_age = int(input("What is your pet's age? "))
 
-pet = Pet(pet_name, pet_breed, pet_age, customer)
+oCustomer.cust_pet = pet(pet_name, pet_breed, pet_age, oCustomer)
 
-customer.cust_pet = pet
+from datetime import datetime
 
 begin_date = datetime.strptime(input("Enter Start date in the format m/d/y: "), "%m/%d/%Y")
 end_date = datetime.strptime(input("Enter End date in the format m/d/y: "), "%m/%d/%Y")
 day_rate = float(input("Enter the day rate: "))
 
-appointment = Appointment(customer)
-appointment.set_appointment(begin_date, end_date, day_rate)
+oCustomer.cust_pet.appointment.set_appointment(begin_date, end_date, day_rate)
 
-customer.cust_pet.appointment = appointment
+print(oCustomer.return_bill())
 
-customer.return_bill()
+oCustomer.make_payment(oCustomer.balance)
 
-customer.make_payment()
-
-customer.return_bill()
+print(oCustomer.return_bill())
